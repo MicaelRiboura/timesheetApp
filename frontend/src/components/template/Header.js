768e83cd281cf1e06d06a3fb78b30aa77d5f2cd8
MicaelRiboura <micaelribouradossantos@gmail.com>
2021-10-22 18:49:00 -0300

    Criação do componente header

--- frontend/src/components/template/Header.js
+++ frontend/src/components/template/Header.js
@@ -1,23 +1,27 @@
+import { ClockOutline, UserOutline, Logout } from "heroicons-react";
+
 const Header = () => {
     return (
         <header className="bg-white shadow-lg w-screen py-4">
-            <div className="px-8 flex justify-between items-center">
-                <div>
+            <div className="xl:px-12 flex justify-between items-center">
+                <div className="flex items-center space-x-4">
                     <div className="logo">
-
+                        <img className="cursor-pointer" src="/assets/images/logo.svg" width="80px" alt="logo timesheet" />
                     </div>
                     <nav>
-                        <ul className="flex items-center">
+                        <ul className="flex items-center space-x-4">
                             <li>
                                 <a href="">
-                                    <p className="p-2">
+                                    <p className="p-2 flex items-center space-x-1 text-blue-900 hover:bg-gray-50 transition rounded-lg font-semibold">
+                                        <ClockOutline className="h-5" />
                                         <span>Lista de Horários</span>
                                     </p>
                                 </a>
                             </li>
                             <li>
                                 <a href="">
-                                    <p className="p-2">
+                                    <p className="p-2 flex items-center space-x-1 text-blue-900 hover:bg-gray-50 transition rounded-lg font-semibold">
+                                        <UserOutline className="h-5" />
                                         <span>Cadastrar Funcionário</span>
                                     </p>
                                 </a>
@@ -26,7 +30,8 @@ const Header = () => {
                     </nav>
                 </div>
                 <div className="p-4">
-                   <div className="">
+                   <div className="flex items-center space-x-1 cursor-pointer font-semibold text-red-900">
+                       <Logout className="h-5" />
                        <span>Sair</span>
                    </div>
                 </div>
