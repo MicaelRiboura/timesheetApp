781566fdb23361a1737dae2df63bf4e30abdd9c8
MicaelRiboura <micaelribouradossantos@gmail.com>
2021-10-27 07:48:13 -0300

    Adição de comentários para organizar rotas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,16 +6,16 @@ const OccupationController = require('../controllers/occupation.controller');
 const UserController = require('../controllers/user.controller');
 const authenticate = passportConfig.authenticate()
 
-
+// employees:
 routes.get('/employees', authenticate, EmployeeController.index);
 routes.get('/employees/:id', authenticate, EmployeeController.find);
 routes.post('/employees', authenticate, EmployeeController.store);
 routes.post('/employees/:id', authenticate, EmployeeController.update);
 routes.delete('/employees/:id', authenticate, EmployeeController.destroy);
-
+//occupations:
 routes.post('/occupation', authenticate, OccupationController.store);
 routes.get('/occupation', authenticate, OccupationController.index);
-
+//auth:
 routes.post('/signup', UserController.signup);
 routes.post('/signin', UserController.signin);
 routes.post('/validate-token', UserController.validateToken);
