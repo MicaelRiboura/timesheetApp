5ac1f67e6ae2113566c8312ea93f91e5db912de8
MicaelRiboura <micaelribouradossantos@gmail.com>
2021-10-28 07:50:19 -0300

    Ajuste em erros do console no frontend

--- frontend/src/components/forms/SearchInput.js
+++ frontend/src/components/forms/SearchInput.js
@@ -1,9 +1,9 @@
 import { Search } from "heroicons-react";
 
-const SearchInput = ({ value, placeholder }) => {
+const SearchInput = ({ placeholder }) => {
     return (
         <div className="border border-blue-900 rounded-sm outline-none p-1 flex items-center bg-white">
-            <input type="text" value={value} className="focus:outline-none" placeholder={placeholder} />
+            <input type="text" className="focus:outline-none" placeholder={placeholder} />
             <Search className="h-4 text-blue-900" />
         </div>
     );
