423f40b7d27d7aca065b6dd46e3785111793736a
MicaelRiboura <micaelribouradossantos@gmail.com>
2021-11-17 14:35:55 -0300

    frontend: Adição de novos campos na lista de funcionários

--- frontend/src/pages/historico/historico.page.js
+++ frontend/src/pages/historico/historico.page.js
@@ -1,51 +1,44 @@
-import React from 'react';
-import Header from '../../components/template/Header';
-import Table from '../../components/template/Table';
-import SearchInput from '../../components/forms/SearchInput';
+import React from "react";
+import Header from "../../components/template/Header";
+import Table from "../../components/template/Table";
+import SearchInput from "../../components/forms/SearchInput";
 import { ClockOutline } from "heroicons-react";
 
-
-
 export default function History() {
-
-    return (
+  return (
+    <div>
+      <Header />
+      <div
+        style={{ height: "90vh" }}
+        className=" px-8 mx-auto"
+        style={{ maxWidth: "1200px" }}
+      >
+        <h2 className="py-8 text-4xl text-blue-900 font-semibold">Histórico</h2>
         <div>
-            <Header />
-            <div style={{height: '90vh'}} className=" px-8 mx-auto" style={{maxWidth: '1200px'}}>
-                <h2 className="py-8 text-4xl text-blue-900 font-semibold">Histórico</h2>
-                <div>
-                    <h2>Nome</h2>
-                    <p>CPF: 111.111.111-11</p>
-                    <p>Cargo: Desenvolvedor</p>
-                </div>
-                <div className="py-12 flex items-center space-x-4">
-                   <SearchInput placeholder="Buscar por Dia do Mês" />
-                </div>
-                <Table
-                    labels={['Data', 'Hora de Entrada', 'Hora de Saída']}
-                    registers ={[
-                        [
-                            (
-                                <div className="text-sm text-gray-900">
-                                    28/10/2021
-                                </div>
-                            ),
-                            (
-                                <div className="text-sm font-medium text-green-500 flex items-center space-x-1">
-                                    <ClockOutline className="h-4" />
-                                   <span>8:00</span>
-                                </div>
-                            ),
-                            (
-                                <div className="text-sm font-medium text-green-500 flex items-center space-x-1">
-                                    <ClockOutline className="h-4" />
-                                    <span>18:00</span>
-                                </div>
-                            ),
-                        ],
-                    ]}
-                />
-            </div>
+          <h2>Nome</h2>
+          <p>CPF: 111.111.111-11</p>
+          <p>Cargo: Desenvolvedor</p>
         </div>
-    );
+        <div className="py-12 flex items-center space-x-4">
+          <SearchInput placeholder="Buscar por Dia do Mês" />
+        </div>
+        <Table
+          labels={["Data", "Hora de Entrada", "Hora de Saída"]}
+          registers={[
+            [
+              <div className="text-sm text-gray-900">28/10/2021</div>,
+              <div className="text-sm font-medium text-green-500 flex items-center space-x-1">
+                <ClockOutline className="h-4" />
+                <span>8:00</span>
+              </div>,
+              <div className="text-sm font-medium text-green-500 flex items-center space-x-1">
+                <ClockOutline className="h-4" />
+                <span>18:00</span>
+              </div>,
+            ],
+          ]}
+        />
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/home/home.page.js
+++ frontend/src/pages/home/home.page.js
@@ -1,84 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../../components/template/Header';
-import Table from '../../components/template/Table';
-import SearchInput from '../../components/forms/SearchInput';
+import React, { useEffect, useState } from "react";
+import Header from "../../components/template/Header";
+import Table from "../../components/template/Table";
+import SearchInput from "../../components/forms/SearchInput";
 import { ClipboardCheck } from "heroicons-react";
-import { Link } from 'react-router-dom';
-import { list } from '../../services/employee.service';
-import { useAuth } from '../../hooks/auth.hook';
+import { Link } from "react-router-dom";
+import { list } from "../../services/employee.service";
+import { useAuth } from "../../hooks/auth.hook";
 
 export default function Home() {
+  const [employees, setEmployees] = useState([]);
+  const { setSigned } = useAuth();
 
-    const [employees, setEmployees] = useState([]);
-    const { setSigned } = useAuth();
-
-    const loadEmployees = async () => {
-            try {
-                const responseEmployees = await list();
-                console.log('employees: ', responseEmployees);
-                console.log('autorizado');
-                setEmployees(responseEmployees.data);
-            } catch(error) {
-                console.log('Não autorizado');
-                console.log("Erro: ", error);
-                setSigned(false);
-            }
+  const loadEmployees = async () => {
+    try {
+      const responseEmployees = await list();
+      console.log("employees: ", responseEmployees);
+      console.log("autorizado");
+      setEmployees(responseEmployees.data);
+    } catch (error) {
+      console.log("Não autorizado");
+      console.log("Erro: ", error);
+      setSigned(false);
     }
+  };
 
-    useEffect(() => {
-        loadEmployees();
-    }, []);
+  useEffect(() => {
+    loadEmployees();
+  }, []);
 
-    return (
-        <div>
-            <Header />
-            <div style={{height: '90vh'}} className=" px-8 mx-auto" style={{maxWidth: '1200px'}}>
-                <h2 className="py-8 text-4xl text-blue-900 font-semibold">Funcionários</h2>
-                <div className="py-12 flex items-center space-x-4">
-                   <SearchInput placeholder="Buscar por CPF" />
-                </div>
-                <Table
-                    labels={['CPF', 'Nome', 'Cargo', 'Horário de Entrada', 'Horário de Saída', 'Ver histórico']}
-                    registers={employees.map(employee => (
-                                [
-                                    (
-                                        <div className="text-sm text-gray-900">
-                                            {employee.socialId}
-                                        </div>
-                                    ),
-                                    (
-                                        <div className="text-sm font-medium text-gray-900">
-                                            {employee.name}
-                                        </div>
-                                    ),
-                                    (
-                                        <div className="text-sm text-blue-400">
-
-                                        </div>
-                                    ),
-                                    (
-                                        <div className="text-sm text-gray-900 font-semibold text-center">
-                                            
-                                        </div>
-                                    ),
-                                    (
-                                        <div className="text-sm text-gray-900 font-semibold text-center">
-                                        
-                                        </div>
-                                    ),
-                                    (
-                                        <Link to="/funcionario/historico">
-                                            <div className="flex items-center text-green-500 cursor-pointer">
-                                                <ClipboardCheck className="h-4" />
-                                                <span>Histórico</span>
-                                            </div>
-                                        </Link>
-                                    ),
-                                ]
-                            )
-                        )}
-                />
-            </div>
+  return (
+    <div>
+      <Header />
+      <div
+        style={{ height: "90vh" }}
+        className=" px-8 mx-auto"
+        style={{ maxWidth: "1200px" }}
+      >
+        <h2 className="py-8 text-4xl text-blue-900 font-semibold">
+          Funcionários
+        </h2>
+        <div className="py-12 flex items-center space-x-4">
+          <SearchInput placeholder="Buscar por CPF" />
         </div>
-    );
+        <Table
+          labels={[
+            "CPF",
+            "Nome",
+            "Cargo",
+            "Horário de Entrada",
+            "Horário de Saída",
+            "Ver histórico",
+          ]}
+          registers={employees.map((employee) => [
+            <div className="text-sm text-gray-900">{employee.socialId}</div>,
+            <div className="text-sm font-medium text-gray-900">
+              {employee.name}
+            </div>,
+            <div className="text-sm text-blue-400">
+              {employee.occupation.name}
+            </div>,
+            <div className="text-sm text-gray-900 font-semibold text-center">
+              {employee.occupation.time_in}
+            </div>,
+            <div className="text-sm text-gray-900 font-semibold text-center">
+              {employee.occupation.time_out}
+            </div>,
+            <Link to="/funcionario/historico">
+              <div className="flex items-center text-green-500 cursor-pointer">
+                <ClipboardCheck className="h-4" />
+                <span>Histórico</span>
+              </div>
+            </Link>,
+          ])}
+        />
+      </div>
+    </div>
+  );
 }
