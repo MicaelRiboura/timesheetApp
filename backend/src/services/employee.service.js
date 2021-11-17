10008457641a22cc14bbb6f247a56dd842651ce8
MicaelRiboura <micaelribouradossantos@gmail.com>
2021-11-17 14:29:42 -0300

    backend: Refatoração da lista de funcionários

--- backend/src/services/employee.service.js
+++ backend/src/services/employee.service.js
@@ -1,53 +1,75 @@
-const Model = require('../models/employee.model');
+const Model = require("../models/employee.model");
+const occupationService = require("./occupation.service");
 
 module.exports = {
-    async index () {
-        try {
-           return  await Model.findAll();
-        } catch( erro ) {
-            console.log(erro);
-            return erro;
-        }
-    },
+  async index() {
+    try {
+      let employees = await Model.findAll();
+      let occupationsIds = employees.map((e) => e.occupation_id);
+      employees = employees.map((e) => JSON.parse(JSON.stringify(e)));
 
-    async find({ id }){
-        try{
-            return await Model.findOne({ where: { id }});
-        }catch( erro ){
-            console.log( erro )
-            return erro;
-        }
-    },
-    
-    async store ({ name, socialId, occupation_id }) {
-        try {
-            return await Model.create({ name, socialId, occupation_id });
-        } catch( erro ) {
-            console.log(erro);
-            return erro;
-        }
-    },
+      const occupations = await occupationService.listForEmployees(
+        occupationsIds
+      );
 
-    async update ({id, name, socialId, occupation_id}) {
-        try {
-            return await Model.update({ 
-                name, 
-                socialId, 
-                occupation_id 
-            }, {where: {id}});
+      employees.forEach((e) => {
+        occupations.forEach((o) => {
+          if (e.occupation_id == o.id) {
+            if (e.occupation == null) e.occupation = [];
+            const oParsed = JSON.parse(JSON.stringify(o));
+            e.occupation = oParsed;
+            delete e.occupation_id;
+          }
+        });
+      });
 
-        } catch( erro ) {
-            console.log(erro);
-            return erro;
-        }
-    },
-
-    async destroy ({ id }) {
-        try {
-            return await Model.destroy({where: {id}});
-        } catch( erro ) {
-            console.log(erro);
-            return erro;
-        }
+      return employees;
+    } catch (erro) {
+      console.log(erro);
+      return erro;
     }
-}
+  },
+
+  async find({ id }) {
+    try {
+      return await Model.findOne({ where: { id } });
+    } catch (erro) {
+      console.log(erro);
+      return erro;
+    }
+  },
+
+  async store({ name, socialId, occupation_id }) {
+    try {
+      return await Model.create({ name, socialId, occupation_id });
+    } catch (erro) {
+      console.log(erro);
+      return erro;
+    }
+  },
+
+  async update({ id, name, socialId, occupation_id }) {
+    try {
+      return await Model.update(
+        {
+          name,
+          socialId,
+          occupation_id,
+        },
+        { where: { id } }
+      );
+    } catch (erro) {
+      console.log(erro);
+      return erro;
+    }
+  },
+
+  async destroy({ id }) {
+    try {
+      return await Model.destroy({ where: { id } });
+    } catch (erro) {
+      console.log(erro);
+      return erro;
+    }
+  },
+};
--- backend/src/services/occupation.service.js
+++ backend/src/services/occupation.service.js
@@ -1,24 +1,38 @@
-const Model = require('../models/occupation.model');
+const Model = require("../models/occupation.model");
+const { Op } = require("sequelize");
 
 module.exports = {
-    async index(){
-        try{
-           return await Model.findAll();
-
-        } catch( erro ) {
-            console.log(erro);
-            return erro;
-        }
-    },
-    
-    async store({ name, time_in, time_out }) {
-        try{
-            return await Model.create({ name, time_in, time_out});
-
-        } catch( erro ) {
-            console.log(erro);
-            return erro;
-        }
-
+  async index() {
+    try {
+      return await Model.findAll();
+    } catch (erro) {
+      console.log(erro);
+      return erro;
     }
+  },
+
+  async store({ name, time_in, time_out }) {
+    try {
+      return await Model.create({ name, time_in, time_out });
+    } catch (erro) {
+      console.log(erro);
+      return erro;
+    }
+  },
+
+  async listForEmployees(idsList) {
+    let list = [];
+    if (Array.isArray(idsList)) {
+      list = idsList;
+    } else {
+      list.push(idsList);
+    }
+
+    try {
+      return await Model.findAll({ where: { id: { [Op.in]: list } } });
+    } catch (erro) {
+      console.log(erro);
+      return erro;
+    }
+  },
 };
